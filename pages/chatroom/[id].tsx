f2053e20889d458b5f33351ec585f88cc8d80945
Ernesto Resende <dev@ernestoresende.com>
2021-10-18 06:56:19 -0300

    chore: updated api endpoint token server

--- pages/chatroom/[id].tsx
+++ pages/chatroom/[id].tsx
@@ -12,17 +12,23 @@ const ChatRoomPage: NextPage = () => {
   const { globalState, globalDispatch } = React.useContext(Context);
   const [isLoading, setIsLoading] = React.useState(true);
 
-  /* Retrieves the JWT token from the token server, which will be stored in
-  the global state and used to connect with the room */
+  /* Retrieves the JWT tokens (conversations service, and video service) from the token server, 
+  which will be stored in the global state and used to connect with the room and the conversation instance */
   React.useEffect(() => {
-    api
-      .get(`?identity=${globalState.userName}&room=${globalState.roomName}`)
-      .then((res) => {
-        globalDispatch({ type: 'SET_USER_TOKEN', payload: res.data });
-        setIsLoading(false);
-      })
-      .catch((err) => console.warn(err));
-  }, [globalDispatch, globalState.userName, globalState.roomName]);
+    const getAccessTokens = async () => {
+      await api
+        .get(`/video-token?identity=${globalState.userName}&room=${globalState.roomName}`)
+        .then((res) => {
+          globalDispatch({ type: 'SET_USER_TOKEN', payload: res.data });
+        })
+        .catch((err) => console.warn(err));
+      setIsLoading(false);
+    };
+
+    getAccessTokens();
+  }, []);
+
+  console.log(globalState);
 
   return (
     <>
